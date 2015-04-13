af9fef77e26e89138b95badce89377184b639b51
rewinn <rachel.winn@me.com>
2015-04-13 13:54:05 -0500

    jQuery mobile CDN / hid "loading"

--- js/index.js
+++ js/index.js
@@ -1,34 +1,15 @@
+$.mobile.loading().hide();
+
 //SHOW VIDEO 
 $(window).on('keydown', function(e) {
   $('[data-key="' + e.which + '"]').show();
+  $('#letter-key').html(String.fromCharCode(event.keyCode)).show();
 });
 
 $(window).on('keyup', function(e) {
   $('[data-key="' + e.which + '"]').hide();
+  $('#letter-key').html(String.fromCharCode(event.keyCode)).hide();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
