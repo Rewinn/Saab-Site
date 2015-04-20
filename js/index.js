4b89c2b9bf36d904ca9f51b863ad2b004356bd8a
rewinn <rachel.winn@me.com>
2015-04-20 16:05:15 -0500

    mobile audio working in Chrome
    Also added .ogg file

--- js/index.js
+++ js/index.js
@@ -1,55 +1,28 @@
-$.mobile.loading().hide();
+$(document).ready(function() {
+    //VIDEO DESKTOP
+    $(window).on('keydown', function(e) {
+        $('[data-key="' + e.which + '"]').show();
+        $('#letter-key').html(String.fromCharCode(event.keyCode)).show(); 
+    });
 
+    $(window).on('keyup', function(e) {
+        $('[data-key="' + e.which + '"]').hide();
+        $('#letter-key').html(String.fromCharCode(event.keyCode)).hide();
+    });
 
-//SHOW VIDEO DESKTOP
-$(window).on('keydown', function(e) {
-  $('[data-key="' + e.which + '"]').show();
-  $('#letter-key').html(String.fromCharCode(event.keyCode)).show();				
+    //VIDEO MOBILE
+    var hammertime = new Hammer(document.body);
+
+    hammertime.on('tap', function(evt) {
+        $('#music').trigger("play");
+        var randomGfy = Math.floor(Math.random() * $('.gfy').length);
+        $('.gfy').hide().eq(randomGfy).show();      
+    });
 });
 
-$(window).on('keyup', function(e) {
-  $('[data-key="' + e.which + '"]').hide();
-  $('#letter-key').html(String.fromCharCode(event.keyCode)).hide();
-});
-
-
-//SHOW VIDEO MOBILE
-var tapStart = function() {
-    var randomGfy = Math.floor(Math.random() * $('.gfy').length);
-    $('.gfy').hide().eq(randomGfy).show();      
-}
-
-var tapEnd = function() {
-    $(randomGfy).hide();
-}
-
-function is_touch_device() {
-    return (('ontouchstart' in window)
-    || (navigator.MaxTouchPoints > 0)
-    || (navigator.msMaxTouchPoints > 0));
-}
-
-var init = function() {
-    $(window).bind('touchstart',tapStart).bind('touchend',tapEnd);  
-}
-
-init();
 
 
 
-
-
-
-
-
-
-//do not delete.  this works kinda
-/*
-$(window).on( "click", function() {
-    var randomGfy = Math.floor(Math.random() * $('.gfy').length);
-    $('.gfy').hide().eq(randomGfy).show();
-});
-*/
 
 
 
